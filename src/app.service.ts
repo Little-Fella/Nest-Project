657518f6c7925d6a1264d041855750c446753d02
Rayden5478 <sashamodestov556@gmail.com>
2025-05-12 17:04:44 +0300

    repositories-added

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,43 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Review } from './reviews/review.entity';
 
 @Injectable()
 export class AppService {
+  constructor(
+    @InjectRepository(Review)
+    private reviewsRepository: Repository<Review>,
+  ) {}
+
   getHello(): string {
-    return 'Hello World!';
+    return 'Hello from AppService!';
+  }
+
+  findAll(): Promise<Review[]> {
+    return this.reviewsRepository.query('SELECT * FROM reviews');
+  }
+
+  findOne(id: number): Promise<Review> {
+    return this.reviewsRepository.query('SELECT * FROM reviews WHERE id = $1', [id]);
+  }
+
+  create(review: Partial<Review>): Promise<Review> {
+    return this.reviewsRepository.query(
+      'INSERT INTO reviews (patient_id, dentist_id, rating, comment) VALUES ($1, $2, $3, $4) RETURNING *',
+      [review.patientId, review.dentistId, review.rating, review.comment],
+    );
+  }
+
+  async update(id: number, review: Partial<Review>): Promise<Review> {
+    await this.reviewsRepository.query(
+      'UPDATE reviews SET patient_id = $1, dentist_id = $2, rating = $3, comment = $4 WHERE id = $5',
+      [review.patientId, review.dentistId, review.rating, review.comment, id],
+    );
+    return this.findOne(id);
+  }
+
+  async remove(id: number): Promise<void> {
+    await this.reviewsRepository.query('DELETE FROM reviews WHERE id = $1', [id]);
   }
 }
